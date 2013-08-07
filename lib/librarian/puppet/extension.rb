cc2d71a092353aa12ba51962fcd40d276d61e575
rsweet <rob@ldg.net>
2013-08-07 14:40:22 -0400

    conflicts

--- lib/librarian/puppet/extension.rb
+++ lib/librarian/puppet/extension.rb
@@ -7,41 +7,6 @@ module Librarian
 
   end
 end
-<<<<<<< HEAD
-
-module Librarian
-  class Dependency
-    class Requirement
-      def initialize(*args)
-        args = initialize_normalize_args(args)
-        self.backing = Gem::Requirement.create(puppet_to_gem_versions(args))
-      end
-
-      def puppet_to_gem_versions(args)
-        args.map do |arg|
-          case arg
-          when Array
-            arg.map { |v| puppet_to_gem_version(v) }
-          when String
-            puppet_to_gem_version(arg)
-          else
-            # Gem::Requirement, do nothing
-            arg
-          end
-        end
-      end
-
-      # convert Puppet '1.x' versions to gem supported versions '~>1.0'
-      # http://docs.puppetlabs.com/puppet/2.7/reference/modules_publishing.html
-      def puppet_to_gem_version(version)
-        matched = /(.*)\.x/.match(version)
-        matched.nil? ? version : "~>#{matched[1]}.0"
-      end
-    end
-  end
-end
-||||||| merged common ancestors
-=======
 
 module Librarian
   class Dependency
@@ -80,4 +45,4 @@ module Librarian
     end
   end
 end
->>>>>>> maestrodev/tons-of-fixes
+
